d84d249a36ab08e4bfd30fd2772fbee132ed95e6
jjieunyun <lesson0186@gmail.com>
2022-04-06 22:08:17 +0900

    22-04-06

--- components/contents-component.js
+++ components/contents-component.js
@@ -1,18 +1,11 @@
 Vue.component('contents-component',{
+
     template : 
     `<div>
         <div class="swiper mySwiper">
-            <div class="contentsText"><h2><slot></slot></h2></div>
+            <div class="contentsText"><h2><slot name="title"></slot></h2></div>
         <div class="swiper-wrapper">
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
-            <div class="swiper-slide"><img src="./img/book1.jpg" alt=""></div>
+            <slot name="div"></slot>
         </div>
             <div class="swiper-button-next"></div>
             <div class="swiper-button-prev"></div>
@@ -25,4 +18,4 @@ Vue.component('contents-component',{
 
         }
     }
-});                
+});   
